6912ca14a9b6db9aa7783064b6a09cca03774a3a
mid <1162564694@qq.com>
2019-01-18 13:50:15 +0800

    Initial commit

---
+++ app/src/main/java/com/mid/component/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mid.component.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
