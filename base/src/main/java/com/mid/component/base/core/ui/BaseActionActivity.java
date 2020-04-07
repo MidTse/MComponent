54e6d3cd6c4c2a3dc743b5bc5b015b6d30c99e07
Mid <903301689@qq.com>
2020-04-07 13:39:59 +0800

    Dependency changes 1.升级至com.android.tools.build:gradle:3.6.2 2.升级至com.billy.android:gloading:1.1.0
    Bug Fixes
    1.修复混淆后MvpArms报throw with null exception

--- base/src/main/java/com/mid/component/base/core/ui/BaseActionActivity.java
+++ base/src/main/java/com/mid/component/base/core/ui/BaseActionActivity.java
@@ -3,14 +3,26 @@ package com.mid.component.base.core.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.InflateException;
 
 import com.gyf.immersionbar.ImmersionBar;
-import com.jess.arms.base.BaseActivity;
+import com.jess.arms.base.delegate.IActivity;
+import com.jess.arms.integration.cache.Cache;
+import com.jess.arms.integration.cache.CacheType;
+import com.jess.arms.integration.lifecycle.ActivityLifecycleable;
 import com.jess.arms.mvp.IPresenter;
 import com.jess.arms.mvp.IView;
+import com.jess.arms.utils.ArmsUtils;
 import com.mid.component.base.utils.ShowUtils;
 import com.trello.rxlifecycle2.android.ActivityEvent;
 
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.Subject;
 import timber.log.Timber;
 
 /**
@@ -22,9 +34,91 @@ import timber.log.Timber;
  *     version : 0.1.0
  * </pre>
  */
-public abstract class BaseActionActivity<P extends IPresenter> extends BaseActivity<P> implements IView, ICommit<ActivityEvent> {
+public abstract class BaseActionActivity<P extends IPresenter> extends AppCompatActivity implements IView, ICommit<ActivityEvent>, ActivityLifecycleable, IActivity {
+    protected final String TAG = this.getClass().getSimpleName();
+    private final BehaviorSubject<ActivityEvent> mLifecycleSubject = BehaviorSubject.create();
+    private Cache<String, Object> mCache;
+    private Unbinder mUnbinder;
+
+    @Inject
+    @Nullable
+    protected P mPresenter;//如果当前页面逻辑简单, Presenter 可以为 null
 
     protected ImmersionBar mImmersionBar;
+
+    @NonNull
+    @Override
+    public synchronized Cache<String, Object> provideCache() {
+        if (mCache == null) {
+            mCache = ArmsUtils.obtainAppComponentFromContext(this).cacheFactory().build(CacheType.ACTIVITY_CACHE);
+        }
+        return mCache;
+    }
+
+    @NonNull
+    @Override
+    public final Subject<ActivityEvent> provideLifecycleSubject() {
+        return mLifecycleSubject;
+    }
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            int layoutResID = initView(savedInstanceState);
+            //如果initView返回0,框架则不会调用setContentView(),当然也不会 Bind ButterKnife
+            if (layoutResID != 0) {
+                setContentView(layoutResID);
+                //绑定到butterknife
+                mUnbinder = ButterKnife.bind(this);
+            }
+        } catch (Exception e) {
+            if (e instanceof InflateException) throw e;
+            e.printStackTrace();
+        }
+        initData(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mUnbinder != null && mUnbinder != Unbinder.EMPTY)
+            mUnbinder.unbind();
+        this.mUnbinder = null;
+        if (mPresenter != null)
+            mPresenter.onDestroy();//释放资源
+        this.mPresenter = null;
+        hideCommiting();
+    }
+
+
+    /**
+     * 是否使用 EventBus
+     * Arms 核心库现在并不会依赖某个 EventBus, 要想使用 EventBus, 还请在项目中自行依赖对应的 EventBus
+     * 现在支持两种 EventBus, greenrobot 的 EventBus 和畅销书 《Android源码设计模式解析与实战》的作者 何红辉 所作的 AndroidEventBus
+     * 确保依赖后, 将此方法返回 true, Arms 会自动检测您依赖的 EventBus, 并自动注册
+     * 这种做法可以让使用者有自行选择三方库的权利, 并且还可以减轻 Arms 的体积
+     *
+     * @return 返回 {@code true} (默认为使用 {@code true}), Arms 会自动注册 EventBus
+     */
+    @Override
+    public boolean useEventBus() {
+        return true;
+    }
+
+    /**
+     * 这个Activity是否会使用Fragment,框架会根据这个属性判断是否注册{@link android.support.v4.app.FragmentManager.FragmentLifecycleCallbacks}
+     * 如果返回false,那意味着这个Activity不需要绑定Fragment,那你再在这个Activity中绑定继承于 {@link com.jess.arms.base.BaseFragment} 的Fragment将不起任何作用
+     *
+     * @return
+     */
+    @Override
+    public boolean useFragment() {
+        return true;
+    }
+
+
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
@@ -34,6 +128,7 @@ public abstract class BaseActionActivity<P extends IPresenter> extends BaseActiv
         }
 
     }
+
 
     @Override
     public void showLoading() {
@@ -65,11 +160,6 @@ public abstract class BaseActionActivity<P extends IPresenter> extends BaseActiv
         ShowUtils.dismissLoadingDialog();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        hideCommiting();
-    }
 
     /**
      * 初始化系统状态栏
